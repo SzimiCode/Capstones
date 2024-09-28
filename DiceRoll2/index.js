1febc80276ba855b6be7952ddc05ce9891554c23
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-09-28 15:43:33 +0200

    foldery

--- DiceRoll2/index.js
+++ DiceRoll2/index.js
@@ -8,23 +8,12 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-var week = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    const day = new Date("2024-09-28T10:58:30");
-    var weekday = day.getDay();
-    
-    if (weekday < 6 || weekday > 0){
-        week = "week";
-    }
-    else{
-        week = "weekend";
-    }
-    console.log(week);
     res.render("index.ejs", {
-        dayType: week,
+
     });
   });
 
