da7689050af4402e33b40ff15aa53d8aec8557c1
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-09-28 14:28:55 +0200

    gowno

--- DiceRoll(2nd Site)/index.js
+++ DiceRoll(2nd Site)/index.js
@@ -7,6 +7,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
 res.render("index.ejs");
 });
